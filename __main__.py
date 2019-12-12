956ef3cc2c3feb9f35e7c76bb2b776e304d7e170
Alex Dikelsky <alexnader.dikelsky@gmail.com>
2019-12-11 21:44:41 -0600

    Added tests

--- __main__.py
+++ __main__.py
@@ -23,6 +23,41 @@ master.add_course( \
         "MUS", "275"))
 #
 
+a = course.Course("Shad", "1:30 MWF", "Olin 202", 24, \
+        "F2019", "A", master.find_course( \
+        master_course.MasterCourse("Algorithms & Data Structures", \
+        "CS", "160"))
+        )
 
-for c in master.course_list:
-    print(c)
+b = course.Course("Shelly", "8:00 MWF", "Valders 206", 250, \
+        "S2018", "B", master.find_course( \
+        master_course.MasterCourse("Introduction to Health", \
+        "HLTH", "100", 1))
+        )
+
+c = course.Course("Guzman", "11:00 MWF", "CFA 123", 1, \
+        "S2020", "A", master.find_course( \
+        master_course.MasterCourse("History of Jazz",
+        "MUS", "275"))
+        )
+
+i = student.Student("Alex Dikelsky", 404191)
+j = student.Student("Laura Bailey", 12345)
+
+a.add_student(i)
+a.add_student(j)
+b.add_student(i)
+b.add_student(j)
+c.add_student(i)
+c.add_student(j)
+
+
+for c in i.courses:
+    print("Alex: ", c)
+print()
+for c in j.courses:
+    print("Laura: ", c)
+
+
+#for c in master.course_list:
+#    print(c)
