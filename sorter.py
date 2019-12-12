8328bade609ad98842205250eadb3eb3b69a7020
Alex Dikelsky <alexnader.dikelsky@gmail.com>
2019-12-11 21:59:40 -0600

    Used sorter from previous problem

--- sorter.py
+++ sorter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-def bubblesort(l):
-    i = 0
+import random
+
+def bubblesort(l): #O(n²)
+    i = 0  
     while i < len(l) - 1:
         j = 0
         while j < len(l)-1:
@@ -12,6 +14,46 @@ def bubblesort(l):
             j += 1
         i += 1
     return l
+
+def mergesort(alist):  #O(log(n) * n)
+    if len(alist) > 1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergesort(lefthalf)
+        mergesort(righthalf)
+
+        left__index = 0
+        right_index = 0
+        both__index = 0
+
+        while left__index < len(lefthalf) and right_index < len(righthalf):
+            if lefthalf[left__index] < righthalf[right_index]:
+                alist[both__index] = lefthalf[left__index]
+                left__index += 1
+            else:
+                alist[both__index] = righthalf[right_index]
+                right_index += 1
+            both__index += 1
+
+        while left__index < len(lefthalf):
+            alist[both__index] = lefthalf[left__index]
+            left__index += 1
+            both__index += 1
+
+        while right_index < len(righthalf):
+            alist[both__index] = righthalf[right_index]
+            right_index += 1
+            both__index += 1
+
+    return alist
+
+#d = [5, 2, 9, 1, 7, 3]
+#d=[2, 3]
+#d = ["a", "b", "d", "r", "c"]
+#
+#print(mergesort(d))
 
 #f = [2, 3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 #f = ["a", "f", "c"]
